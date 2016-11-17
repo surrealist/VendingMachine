32c26b991aab9f28f8bc54b4f0f71bd277956cfe
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2016-11-17 16:47:23 +0700

    Add Sales Report
    In Admin area.

---
+++ JL.VendingMachines/Areas/Admin/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using JL.VendingMachines.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JL.VendingMachines.Areas.Admin.Controllers {
+  public class ReportController : Controller {
+    
+    private MachineDb db = new MachineDb();
+
+    protected override void Dispose(bool disposing) {
+      if (disposing) {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    public ActionResult Sales(int? ProductId) {
+      var q = from item in db.SaleItems
+              select item;
+      if (ProductId != null) {
+        q = q.Where(i => i.Product.Id == ProductId);
+      }
+
+      ViewBag.ProductId = new SelectList(db.Products, "Id", "Name");
+      ViewBag.CurrentProductId = ProductId;
+      return View(q.ToList());
+    }
+  }
+}
